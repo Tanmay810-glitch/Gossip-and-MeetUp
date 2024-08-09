7da44dcfd5d0f57572dd34dc85d6befc2d2517d7
Tanmay810-glitch <theelitehacker32@gmail.com>
2024-08-09 11:20:57 +0530

    Added a follow button

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -12,7 +12,8 @@ const UserCard = ({ user } : UserCardProps) => {
       <img src={user.imageUrl} className='rounded-full' width={80} height={80} alt="" />
       <p className='text-lg font-bold'>{user.username}</p>
       <p className='text-light-3'>{user.email}</p>
-      <p className='text-sm'>{user.bio}</p>
+      <p className='text-sm'>{user?.bio?.slice(0,55).concat("...")}</p>
+      <button className='border border-primary-500 p-2 px-3 rounded-full hover:bg-primary-500 hover:text-white transition-all duration-500'>Follow</button>
     </div>
   )
 }
